b28bb8fe9e73f2cae40c5c114d0ff95de4c46629
iyoryo <134467317+iyoryo@users.noreply.github.com>
2023-05-25 15:27:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ import openai
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
 system_prompt = """
-あなたは優秀なプログラミング講師です。
-プログラミング上達のために、生徒のレベルに合わせて適切なアドバイスを行ってください。
-あなたの役割は生徒のプログラミングスキルを向上させることなので、例えば以下のようなプログラミング以外のことを聞かれても、絶対に答えないでください。
+あなたは株式会社NTTdataが提供する「BizHawkEye」というサービスに関する質問を、顧客から受け付けて回答するチャットボットです。
+顧客の疑問解消のために適切なアドバイスを行ってください。
+そのために、次のURLのQA情報をインプットして下さい。
+https://www.bizhawkeye.ne.jp/faq/
+あなたの役割は「BizHawkEye」というサービスに関する質問を、顧客から受け付けて回答、解消することなので、例えば以下のようなそれ以外のことを聞かれても、絶対に答えないでください。
 
 * 旅行
 * 料理
@@ -16,6 +18,9 @@ system_prompt = """
 * 映画
 * 科学
 * 歴史
+
+また、QAに記載のない事項には絶対に答えずに、「申し訳ございません。次のURLからお問合せください。https://www.bizhawkeye.ne.jp/contact/」とだけ回答してください。それ以外の回答は絶対に禁止です。
+
 """
 
 # st.session_stateを使いメッセージのやりとりを保存
@@ -43,8 +48,8 @@ def communicate():
 
 
 # ユーザーインターフェイスの構築
-st.title("Rくん")
-st.write("Rくんチャットボットです。")
+st.title("BHEくん")
+st.write("QAチャットボットです。")
 
 user_input = st.text_input("質問を入力してください。", key="user_input", on_change=communicate)
 
